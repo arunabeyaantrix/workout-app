a162b036fa88920a22541e45dd9976920af4510d
Arun dev <arun.dev@cast-er.com>
2024-10-25 10:38:13 +0530

    Added count down

--- src/data/countdown.ts
+++ src/data/countdown.ts
@@ -3,4 +3,5 @@ export const countDowns = [
   {id: '4', title: 'Home Plan', endDate: '2025-02-01T00:00:00.000Z'},
   {id: '3', title: 'Loan close', endDate: '2025-03-01T00:00:00.000Z'},
   {id: '2', title: 'Bike Day', endDate: '2025-05-22T00:00:00.000Z'}, 
+  {id: '5', title: 'Salary updation Day', endDate: '2024-10-31T00:00:00.000Z'}, 
 ]
